25e678447df62fe04b71ab4f7bebfef6663c4f4b
RevolutionTTT <119401966+RevolutionTTT@users.noreply.github.com>
2025-11-12 15:19:20 +0800

    Delete seed_urls.py

--- seed_urls.py
+++
@@ -1,8 +0,0 @@
-import config
-
-def generate_page_urls():
-    links = config.links
-    base_url = config.base_url
-    for i in range(2, 51):
-        links.append(base_url.format(i))
-    return links
